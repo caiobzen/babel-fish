83a5bfb2fbec399a8f487b3ed34428230a276f79
Chris Steele <csteele@roundarchisobar.com>
2016-10-29 17:23:14 -0200

    Added lame lib

--- Babel Fish/Babel Fish/BFMainViewController.m
+++ Babel Fish/Babel Fish/BFMainViewController.m
@@ -9,6 +9,7 @@
 #import "BFConstants.h"
 #import "BFMainViewController.h"
 #import <AVFoundation/AVFoundation.h>
+#import "lame/lame.h"
 
 @interface BFMainViewController () <AVAudioRecorderDelegate, AVAudioPlayerDelegate>
 @property (nonatomic, strong) IBOutlet UITextView *textView;
@@ -78,8 +79,63 @@
     }
 }
 
+- (void)convertAudio
+{
+    NSString *filePath = [self soundFilePath];
+    
+    NSString *mp3FileName = @"Mp3File";
+    mp3FileName = [mp3FileName stringByAppendingString:@".mp3"];
+    NSString *mp3FilePath = [NSTemporaryDirectory() stringByAppendingPathComponent:mp3FileName];
+    
+    NSLog(@"%@", mp3FilePath);
+    
+    @try {
+        int read, write;
+        
+        FILE *pcm = fopen([filePath cStringUsingEncoding:1], "rb");  //source
+        fseek(pcm, 4*1024, SEEK_CUR);                                   //skip file header
+        FILE *mp3 = fopen([mp3FilePath cStringUsingEncoding:1], "wb");  //output
+        
+        const int PCM_SIZE = 8192;
+        const int MP3_SIZE = 8192;
+        short int pcm_buffer[PCM_SIZE*2];
+        unsigned char mp3_buffer[MP3_SIZE];
+        
+        lame_t lame = lame_init();
+        lame_set_in_samplerate(lame, 44100);
+        lame_set_VBR(lame, vbr_default);
+        lame_init_params(lame);
+        
+        do {
+            read = (int)fread(pcm_buffer, 2*sizeof(short int), PCM_SIZE, pcm);
+            if (read == 0)
+                write = lame_encode_flush(lame, mp3_buffer, MP3_SIZE);
+            else
+                write = lame_encode_buffer_interleaved(lame, pcm_buffer, read, mp3_buffer, MP3_SIZE);
+            
+            fwrite(mp3_buffer, write, 1, mp3);
+            
+        } while (read != 0);
+        
+        lame_close(lame);
+        fclose(mp3);
+        fclose(pcm);
+    }
+    @catch (NSException *exception) {
+        NSLog(@"%@",[exception description]);
+    }
+    @finally {
+        NSLog(@"convert Finished: %@",mp3FilePath);
+//        [self performSelectorOnMainThread:@selector(convertMp3Finish)
+//                               withObject:nil
+//                            waitUntilDone:YES];
+    }
+}
+
 - (IBAction) processAudio:(id) sender {
     [self stopAudio:sender];
+    
+    [self convertAudio];
     
     NSString *service = @"https:/speech.googleapis.com/v1beta1/speech:syncrecognize";
     service = [service stringByAppendingString:@"?key="];
